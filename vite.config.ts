6a27c8d5fb8c17bef42b436ddbd143bda18410b1
Saksham <ashuu.00000000000@gmail.com>
2025-10-09 12:01:11 +0530

    huh

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:'/Hacakthon',
+  base:"/Eco360",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
